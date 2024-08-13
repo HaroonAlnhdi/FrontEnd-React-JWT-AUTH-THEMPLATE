2fd781a60aff57e01aa44731f34a1ba4a0224822
Haroon <you@example.com>
2024-08-13 16:28:14 +0300

    chore: Add SigninForm component and update authService, add sign out

--- src/services/authService.js
+++ src/services/authService.js
@@ -19,4 +19,44 @@ const signup = async (formData) => {
   }
 };
 
-export  { signup,};
+
+
+const signin = async (user) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/signin`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      })
+      const json = await res.json()
+  
+      if (json.token) {
+        localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+  
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+  
+        return user
+      }
+      if (json.err) {
+        throw new Error(json.err)
+      }
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
+  }
+// The getUser function will check localStorage for a token. If one does not exist, null will be returned. If one does exist and it has a user, the getUser function will return that user.
+  const getUser = () =>  {
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    const user = JSON.parse(atob(token.split('.')[1]));
+    return user;
+  }
+  
+  const signout = () => {
+    localStorage.removeItem('token');
+  };
+  
+  export { signup, signin, getUser, signout };
+
+
